561a6122b9d4685f2ae93faf5ae14edd67dda10c
Sethmh82 <seth@statisticbrain.com>
2017-05-01 11:59:48 -0700

    Added a WhileLoop Example

---
+++ 8 While Loop/WhileLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WhileLoop
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Random numberGen = new Random();
+
+			int numberOfAttempts = 0;
+			int attempt = 0;
+
+			while (attempt != 6)
+			{
+				attempt = numberGen.Next(1, 7);
+				Console.WriteLine("Seth rolled: " + attempt + ".");
+				numberOfAttempts++;
+			}
+
+			Console.WriteLine("It took Seth " + numberOfAttempts + " attempts to roll a six.");
+
+			Console.ReadKey();
+
+
+
+		}
+	}
+}
