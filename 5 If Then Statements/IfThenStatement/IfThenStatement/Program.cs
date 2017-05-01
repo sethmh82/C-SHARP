b455fcb3ab4c9350dea534aacc8662c5ca4fd220
Sethmh82 <seth@statisticbrain.com>
2017-04-30 23:13:02 -0700

    Added If Then Statement

---
+++ 5 If Then Statements/IfThenStatement/IfThenStatement/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ForStatements
+
+{
+	class MainClass
+
+	{
+		public static void Main(string[] args)
+
+		{
+
+			Random numberGenerator = new Random();
+
+			int num01 = numberGenerator.Next(1, 11);
+			int num02 = numberGenerator.Next(1, 11);
+
+			Console.WriteLine("What is " + num01 + " times " + num02 + "?");
+
+			int answer = Convert.ToInt32(Console.ReadLine());
+
+			if (answer == num01 * num02)
+
+			{
+
+				int responseIndex = numberGenerator.Next(1, 4);
+
+				switch (responseIndex)
+
+				{
+
+					case 1:
+						Console.WriteLine("Correct, well done!");
+						break;
+					case 2:
+						Console.WriteLine("The answer was correct.");
+						break;
+					default:
+						Console.WriteLine("Have you been practicing?");
+						break;
+				}
+
+			}
+			else
+			{
+				int diff = Math.Abs(answer - (num01 = num02));
+				if (diff == 1)
+				{
+					Console.WriteLine("So close!");
+				}
+				else if (diff <= 10)
+				{
+					Console.WriteLine("You can do better than that!");
+				}
+				else
+				{
+					Console.WriteLine("You're not even close!");
+				}
+
+				Console.ReadKey();
+			}
+		}
+	}
+
+}
