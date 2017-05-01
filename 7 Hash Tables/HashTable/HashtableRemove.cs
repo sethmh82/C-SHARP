c8e7d82f3c8a2539a0113635f7b522f48cfa9e1f
Sethmh82 <seth@statisticbrain.com>
2017-05-01 01:41:47 -0700

    Hash table examples added

---
+++ 7 Hash Tables/HashTable/HashtableRemove.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+public class SampleHashRemove
+{
+
+	public static void Main ()
+	{
+
+		// Creates and initializes a new Hashtable.
+		Hashtable myHT = new Hashtable();
+		myHT.Add("1a", "The");
+		myHT.Add("1b", "quick");
+		myHT.Add("1c", "brown");
+		myHT.Add("2a", "fox");
+		myHT.Add("2b", "jumped");
+		myHT.Add("2c", "over");
+		myHT.Add("3a", "the");
+		myHT.Add("3b", "lazy");
+		myHT.Add("3c", "dog");
+
+		// Displays the Hashtable.
+		Console.WriteLine("The Hashtable initially contains the following:");
+		PrintKeysAndValues(myHT);
+
+		Hashtable myHT2 = new Hashtable();
+		myHT2.Add("Name1", "Mark");
+		myHT2.Add("Name2", "Jim");
+		myHT2.Add("Name3", "Jeff");
+		myHT2.Add("Name4", "Steve");
+		myHT2.Add("Name5", "Jessica");
+		myHT2.Add("Name6", "Oliver");
+		myHT2.Add("Name7", "Lola");
+		myHT2.Add("Name8", "Bob");
+		myHT2.Add("Name9", "Stanley");
+        PrintKeysAndValues2(myHT2);
+		// Removes the element with the key "3b".
+		myHT.Remove("3b");
+		myHT.Remove("2c");
+		myHT.Remove("2a");
+		myHT.Remove("1c");
+		myHT.Remove("1a");
+
+		// Displays the current state of the Hashtable.
+		Console.WriteLine("After removing \"lazy\":");
+		PrintKeysAndValues(myHT);
+	}
+
+
+	public static void PrintKeysAndValues(Hashtable myHT)
+	{
+		foreach (DictionaryEntry de in myHT)
+			Console.WriteLine("    {0}:    {1}", de.Key, de.Value);
+		Console.WriteLine();
+	}
+
+	public static void PrintKeysAndValues2(Hashtable myHT2)
+	{
+	foreach (DictionaryEntry de in myHT2)
+		Console.WriteLine("    {0}:    {1}", de.Key, de.Value);
+	Console.WriteLine();	
+	}
+
+}
---
+++ 7 Hash Tables/HashTable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+public class SampHashVal
+{
+
+	public static void Main2()
+	{
+
+		// Creates and initializes a new Hashtable.
+		Hashtable myHT = new Hashtable();
+		myHT.Add(0, "zero");
+		myHT.Add(1, "one");
+		myHT.Add(2, "two");
+		myHT.Add(3, "three");
+		myHT.Add(4, "four");
+		myHT.Add(5, "seth");
+		myHT.Add(6, "michael");
+
+		// Displays the values of the Hashtable.
+		Console.WriteLine("The Hashtable contains the following values:");
+		PrintIndexAndKeysAndValues(myHT);
+
+		// Searches for a specific key.
+		int myKey = 2;
+		Console.WriteLine("The key \"{0}\" is {1}.", myKey, myHT.ContainsKey(myKey) ? "in the Hashtable" : "NOT in the Hashtable");
+		myKey = 6;
+		Console.WriteLine("The key \"{0}\" is {1}.", myKey, myHT.ContainsKey(myKey) ? "in the Hashtable" : "NOT in the Hashtable");
+
+		// Searches for a specific value.
+		String myValue = "three";
+		Console.WriteLine("The value \"{0}\" is {1}.", myValue, myHT.ContainsValue(myValue) ? "in the Hashtable" : "NOT in the Hashtable");
+	 	myValue = "seth";
+		Console.WriteLine("The value \"{0}\" is {1}.", myValue, myHT.ContainsValue(myValue) ? "in the Hashtable" : "NOT in the Hashtable");
+		myValue = "harden";
+		Console.WriteLine("The value \"{0}\" is {1}.", myValue, myHT.ContainsValue(myValue) ? "in the Hashtable" : "NOT in the Hashtable");
+		myValue = "nine";
+		Console.WriteLine("The value \"{0}\" is {1}.", myValue, myHT.ContainsValue(myValue) ? "in the Hashtable" : "NOT in the Hashtable");
+	}
+
+
+	public static void PrintIndexAndKeysAndValues(Hashtable myHT)
+	{
+		int i = 0;
+		Console.WriteLine("\t-INDEX-\t-KEY-\t-VALUE-");
+		foreach (DictionaryEntry de in myHT)
+			Console.WriteLine("\t[{0}]:\t{1}\t{2}", i++, de.Key, de.Value);
+		Console.WriteLine();
+	}
+
+}
