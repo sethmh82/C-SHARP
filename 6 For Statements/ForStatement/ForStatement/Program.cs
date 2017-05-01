ad9a8fe79593faf7afbc88a97d4f7796578586c1
Sethmh82 <seth@statisticbrain.com>
2017-05-01 01:52:55 -0700

    Added For Example

--- 6 For Statements/ForStatement/ForStatement/Program.cs
+++ 6 For Statements/ForStatement/ForStatement/Program.cs
@@ -6,7 +6,13 @@ namespace ForStatement
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
+
+			for (int i = 1; i <= 100; i++)
+			{
+				Console.WriteLine(i);
+			}
+			Console.ReadKey();
 		}
 	}
 }
